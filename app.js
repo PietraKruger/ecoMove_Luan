a4c1ef3532f4850b6591a56edefa3bdc0f8e1450
Pietra Kruger Andrade <Pietrapikruger11@gmail.com>
2026-09-17 17:08:15 -0300

    saepchef

--- app.js
+++
@@ -1,9 +0,0 @@
-const authBtn = document.getElementById('auth-btn');
-const activityFormSection = document.getElementById('activity-form-section');
-const activityTab = document.getElementById('activity-tab');
-
-authBtn.addEventListener('click', () => {
-activityFormSection.classList.remove('hidden');
-activityTab.removeAttribute('disabled');
-authBtn.textContent = 'Logout';
-});
